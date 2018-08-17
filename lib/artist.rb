0408c7a5cb13b9c60bbed60ae0c9882276ee9497
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-17 16:28:18 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -17,7 +17,7 @@ attr_reader :songs
 
     def new_song(name, genre)
       self.songs << Song.new(name, self, genre)
-      binding.pry
+      # binding.pry
       # self.genres << self.genre
     end
 
