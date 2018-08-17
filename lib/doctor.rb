acd05e4a66399b0427fbee91b80b770ef47d738d
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-17 17:15:19 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -2,6 +2,7 @@ class Doctor
 
   attr_accessor :name
   @@all=[]
+  
     def initialize(name)
       @name=name
       @@all << self
@@ -9,6 +10,10 @@ class Doctor
 
     def new_appointment(date, patient)
       appointment = Appointment.new(patient, self, date)
+    end
+
+    def self.all
+      @@all
     end
 
     def appointments
