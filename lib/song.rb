9c9f0669ff2641b13eea18faf30f56aa40ba573a
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-17 16:59:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,7 @@ attr_accessor :name, :artist, :genre
     def initialize(name, artist, genre)
       @name=name
       @artist=artist
-      @genre=Genre.new(genre)
+      @genre=genre
       @@all << self
     end
 
